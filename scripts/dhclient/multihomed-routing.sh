e7981058d91c310293f37b654c590d6842d9d163
Kristian Evensen <kristian.evensen@gmail.com>
2017-04-22 13:52:47 +0200

    Support dhclient TIMEOUT state
    Support the dhclient TIMEOUT state. The way we will handle this state is
    as follows:
    
    * Check if a table is allocated for the given interface/address.
    * If so, return this table. If not, allocate a new table.
    * Configure as usual.
    
    The reason we don't need to delete any thing is that a delete has either
    already happened, or the address, routes, etc. are still valid.

--- scripts/dhclient/multihomed-routing.sh
+++ scripts/dhclient/multihomed-routing.sh
@@ -4,9 +4,50 @@ ADDR_RULE_PREF=10000
 NW_RULE_PREF=20000
 LO_RULE_PREF=91000
 
+add_multihomed_rules()
+{
+    rt_table=$1
+
+    #set up new routing rules
+    ip -4 rule add from \
+        ${new_ip_address}${new_subnet_mask:+/$new_subnet_mask} \
+        pref ${ADDR_RULE_PREF} lookup ${rt_table}
+    ip -4 rule add from all to \
+        ${new_ip_address}${new_subnet_mask:+/$new_subnet_mask} pref \
+        ${NW_RULE_PREF} lookup ${rt_table}
+    ip -4 rule add from all iif lo lookup ${rt_table} pref \
+        ${LO_RULE_PREF}
+}
+
+delete_multihomed_rules()
+{
+    old_rt_table=$1
+
+    ip -4 rule delete from \
+       ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} \
+       pref ${ADDR_RULE_PREF} lookup ${old_rt_table}
+    ip -4 rule delete from all to \
+        ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} pref \
+        ${NW_RULE_PREF} lookup ${old_rt_table}
+    ip -4 rule delete from all iif lo lookup ${old_rt_table} pref \
+        ${LO_RULE_PREF}
+}
+
+update_addr_route()
+{
+    rt_table=$1
+
+    ip -4 ro delete \
+        ${new_network_number}${new_subnet_mask:+/$new_subnet_mask} \
+        dev ${interface}
+    ip -4 ro add \
+        ${new_network_number}${new_subnet_mask:+/$new_subnet_mask} \
+        dev ${interface} src ${new_ip_address} table ${rt_table}
+}
+
 handle_bound_renew_rebind_reboot()
 {
-    #todo: need to read old table
+    #todo: need to read + release old table
     #todo: need to allocate table here (could be merged with previous)
     rt_table=100
     old_rt_table=100
@@ -26,14 +67,7 @@ handle_bound_renew_rebind_reboot()
             ip -4 addr flush dev ${interface} label ${interface}
 
             #Delete our routing rules for this address
-            ip rule delete from \
-                ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} \
-                pref ${ADDR_RULE_PREF} lookup ${old_rt_table}
-            ip rule delete from all to \
-                ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} pref \
-                ${NW_RULE_PREF} lookup ${old_rt_table}
-            ip rule delete from all iif lo lookup ${old_rt_table} pref \
-                ${LO_RULE_PREF}
+            delete_multihomed_rules $old_rt_table
     fi
 
     if [ -z "$old_ip_address" ] ||
@@ -45,22 +79,10 @@ handle_bound_renew_rebind_reboot()
             dev ${interface} label ${interface}
 
         #remove default address route and add it to the correct table
-        ip -4 ro delete \
-            ${new_network_number}${new_subnet_mask:+/$new_subnet_mask} \
-            dev ${interface}
-        ip -4 ro add \
-            ${new_network_number}${new_subnet_mask:+/$new_subnet_mask} \
-            dev ${interface} src ${new_ip_address} table ${rt_table}
+        update_addr_route $rt_table
 
         #set up new routing rules
-        ip rule add from \
-            ${new_ip_address}${new_subnet_mask:+/$new_subnet_mask} \
-            pref ${ADDR_RULE_PREF} lookup ${rt_table}
-        ip rule add from all to \
-            ${new_ip_address}${new_subnet_mask:+/$new_subnet_mask} pref \
-            ${NW_RULE_PREF} lookup ${rt_table}
-        ip rule add from all iif lo lookup ${rt_table} pref \
-            ${LO_RULE_PREF}
+        add_multihomed_rules $rt_table
 
         if [ -n "$new_interface_mtu" ]; then
             # set MTU
@@ -122,16 +144,91 @@ handle_expire_fail_release_stop()
     #we don't need to handle routes, they are automatically removed when address
     #is flushed
     if [ -n "$old_ip_address" ]; then
-        #remove rules
-        ip rule delete from \
-            ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} \
-            pref ${ADDR_RULE_PREF} lookup ${old_rt_table}
-        ip rule delete from all to \
-            ${old_ip_address}${old_subnet_mask:+/$old_subnet_mask} pref \
-            ${NW_RULE_PREF} lookup ${old_rt_table}
-        ip rule delete from all iif lo lookup ${old_rt_table} pref \
-            ${LO_RULE_PREF}
+        delete_multihomed_rules $old_rt_table
     fi
+}
+
+handle_timeout()
+{
+    #algorithm for timeout handling should be as follows:
+    # * Send an allocation request for this interface, address and address
+    # family.
+    # * Try to configure everything as usual. If adress etc. is already
+    # configured, the different steps will just fail.
+    #There is no chance that the routing table for one interface + address will
+    #move, since we control which table to use
+
+    #todo: need to allocate/read table here
+    rt_table=100
+
+    #as always, we don't care about alias
+    if [ -n "$alias_ip_address" ]; then
+        # flush alias IP
+        ip -4 addr flush dev ${interface} label ${interface}:0
+    fi
+
+    # set IP from recorded lease
+    ip -4 addr add ${new_ip_address}${new_subnet_mask:+/$new_subnet_mask} \
+        ${new_broadcast_address:+broadcast $new_broadcast_address} \
+        dev ${interface} label ${interface}
+
+    # move adress route to correct table
+    update_addr_route $rt_table
+
+    #delete + add multihomed rules, just to be sure
+    #todo: check that there is an old_rt_table
+    add_multihomed_rules $rt_table
+
+    if [ -n "$new_interface_mtu" ]; then
+        # set MTU
+        ip link set dev ${interface} mtu ${new_interface_mtu}
+    fi
+
+    # if there is no router recorded in the lease or the 1st router answers pings
+    if [ -z "$new_routers" ] || ping -q -c 1 "${new_routers%% *}"; then
+        # if we have $new_rfc3442_classless_static_routes then we have to
+        # ignore $new_routers entirely
+        if [ ! "$new_rfc3442_classless_static_routes" ]; then
+            if [ -n "$alias_ip_address" ] &&
+               [ "$new_ip_address" != "$alias_ip_address" ]; then
+            # separate alias IP given => set up the alias IP & add host route to it
+            ip -4 addr add ${alias_ip_address}${alias_subnet_mask:+/$alias_subnet_mask} \
+                dev ${interface} label ${interface}:0
+            ip -4 route add ${alias_ip_address} dev ${interface} >/dev/null 2>&1
+            fi
+
+            # set if_metric if IF_METRIC is set or there's more than one router
+            if_metric="$IF_METRIC"
+            if [ "${new_routers%% *}" != "${new_routers}" ]; then
+            if_metric=${if_metric:-1}
+            fi
+
+            # set default route
+            for router in $new_routers; do
+            ip -4 ro add default via ${router} dev ${interface} \
+                ${if_metric:+metric $if_metric} src ${new_ip_address} \
+                table ${rt_table} >/dev/null 2>&1
+
+            if [ -n "$if_metric" ]; then
+                if_metric=$((if_metric+1))
+            fi
+            done
+        fi
+
+        # update /etc/resolv.conf
+        make_resolv_conf
+    else
+        # flush all IPs from interface
+        ip -4 addr flush dev ${interface}
+
+        #todo: check that there is an old_rt_table
+        #todo: get all tables for one interface?
+        delete_multihomed_rules $old_rt_table
+
+        exit_with_hooks 2
+    fi
+
+    unset reason
 }
 
 case "$reason" in
@@ -144,6 +241,9 @@ case "$reason" in
         ;;
     EXPIRE|FAIL|RELEASE|STOP)
         handle_expire_fail_release_stop
+        ;;
+    TIMEOUT)
+        handle_timeout
         ;;
 esac
 
