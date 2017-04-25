a99998c75b7168ff16d66040c6097ca47e714b69
Kristian Evensen <kristian.evensen@gmail.com>
2017-04-25 21:09:16 +0200

    [SRC] Create JSON request object
    Add code for creating and sending the JSON request object.

--- table_allocator/client/src/table_allocator_client.c
+++ table_allocator/client/src/table_allocator_client.c
@@ -10,6 +10,8 @@
 #include <arpa/inet.h>
 
 #include "table_allocator_client.h"
+
+#include "table_allocator_shared_json.h"
 #include "table_allocator_log.h"
 #include "table_allocator_libuv_helpers.h"
 #include "table_allocator_socket_helpers.h"
@@ -58,13 +60,34 @@ static void client_request_timeout_handle_cb(uv_timer_t *handle)
 
 static void table_allocator_client_send_request(struct tac_ctx *ctx)
 {
-    uint8_t snd_buf[1] = {'a'};
 	struct sockaddr_un remote_addr;
     //this is just a test until we have config files in place
     int32_t retval = -1, sock_fd;
+    struct json_object *req_obj = NULL;
+    const char *json_str;
+    
+    req_obj = table_allocator_shared_json_create_req(ctx->address, ctx->ifname,
+            ctx->tag, ctx->addr_family, TA_SHARE_CMD_REQ);
+
+    if (!req_obj) {
+        if (uv_timer_start(&(ctx->request_timeout_handle),
+                    client_request_timeout_handle_cb, REQUEST_RETRANSMISSION_MS,
+                    0)) {
+            TA_PRINT_SYSLOG(ctx, LOG_CRIT, "Can't start request timer\n");
+		    exit(EXIT_FAILURE);
+	    }
+
+        TA_PRINT_SYSLOG(ctx, LOG_ERR, "Failed to create request json for "
+                "%s-%s-%u\n", ctx->ifname, ctx->address, ctx->addr_family);
+        return;
+    }
+
+    json_str = json_object_to_json_string_ext(req_obj, JSON_C_TO_STRING_PLAIN);
+
+    TA_PRINT_SYSLOG(ctx, LOG_DEBUG, "JSON string: %s %zd\n",
+            json_str, strlen(json_str));
 
     memset(&remote_addr, 0, sizeof(remote_addr));
-
     remote_addr.sun_family = AF_UNIX;
 
     //we use abstract naming, so first byte of path is always \0
@@ -73,9 +96,10 @@ static void table_allocator_client_send_request(struct tac_ctx *ctx)
 
     //todo: I need to use sendto, it seems the diffrent send-methods in libuv
     //expects something that is either sockaddr_in or sockaddr_in6 (investigate)
-    retval = sendto(sock_fd, snd_buf, 1, 0, (const struct sockaddr*) &remote_addr, sizeof(struct sockaddr_un));
+    retval = sendto(sock_fd, json_str, strlen(json_str), 0,
+            (const struct sockaddr*) &remote_addr, sizeof(struct sockaddr_un));
 
-    if (retval < 0 && retval != UV_EAGAIN) {
+    if (retval < 0) {
         TA_PRINT_SYSLOG(ctx, LOG_ERR, "Sending error: %s\n", uv_strerror(retval));
     } else {
         TA_PRINT(ctx->logfile, "Sent %d bytes\n", retval);
@@ -89,7 +113,6 @@ static void table_allocator_client_send_request(struct tac_ctx *ctx)
     if (uv_timer_start(&(ctx->request_timeout_handle),
                 client_request_timeout_handle_cb, REQUEST_RETRANSMISSION_MS, 0)) {
         TA_PRINT_SYSLOG(ctx, LOG_CRIT, "Can't start request timer\n");
-        //do clean-up at least
 		exit(EXIT_FAILURE);
 	}
 }
@@ -98,7 +121,7 @@ static void table_allocator_client_send_request(struct tac_ctx *ctx)
 static void unix_socket_timeout_cb(uv_timer_t *handle)
 {
     int32_t sock_fd = -1;
-    uint8_t success = 1;
+    uint8_t success = 1, addr_family_translated = AF_UNSPEC;
     struct tac_ctx *ctx = handle->data;
     //format is ifname-addr-family. The space for the two "-" comes for free via
     //the additional byte in IFNAMSIZE and INET6_ADDRSTRLEN, family is either 0, 4
@@ -107,8 +130,14 @@ static void unix_socket_timeout_cb(uv_timer_t *handle)
 
     if (uv_fileno((const uv_handle_t*) &(ctx->unix_socket_handle), &sock_fd)
             == UV_EBADF) {
+        if (ctx->addr_family == AF_INET) {
+            addr_family_translated = 4;
+        } else if (ctx->addr_family == AF_INET6) {
+            addr_family_translated = 6;
+        }
+
         snprintf(unix_socket_addr, sizeof(unix_socket_addr),
-                "%s-%s-%u", ctx->ifname, ctx->address, ctx->addr_family);
+                "%s-%s-%u", ctx->ifname, ctx->address, addr_family_translated);
 
         //path will be read from config, stored in ctx
         sock_fd = ta_socket_helpers_create_unix_socket(unix_socket_addr);
@@ -154,7 +183,7 @@ static void usage()
     fprintf(stdout, "\t-s/--syslog: enable logging to syslog (default off)\n");
     fprintf(stdout, "\t-l/--log_path: path to logfile (default stderr)\n");
     fprintf(stdout, "\t-a/--address: address to allocate table for <r>\n");
-    fprintf(stdout, "\t-i/--iface: interface to allocate table for <r>\n");
+    fprintf(stdout, "\t-i/--ifname: interface to allocate table for <r>\n");
     fprintf(stdout, "\t-t/--tag: optional tag to send to server\n");
     fprintf(stdout, "\t-h/--help: this information\n");
 }
@@ -169,7 +198,7 @@ static uint8_t parse_cmd_args(struct tac_ctx *ctx, int argc, char *argv[])
         {"syslog", no_argument, NULL, 's'},
         {"log_path", required_argument, NULL, 'l'},
         {"address", required_argument, NULL, 'a'},
-        {"iface", required_argument, NULL, 'i'},
+        {"ifname", required_argument, NULL, 'i'},
         {"tag", required_argument, NULL, 't'},
         {"destination", required_argument, NULL, 'd'},
         {"help", no_argument, NULL, 'h'},
@@ -220,24 +249,24 @@ static uint8_t parse_cmd_args(struct tac_ctx *ctx, int argc, char *argv[])
     }
 
     if (strlen(tag) >= MAX_TAG_SIZE) {
-        fprintf(stderr, "Tag name too long (%zd > %u\n", strlen(tag),
-                MAX_TAG_SIZE);
+        fprintf(stderr, "Tag name too long (%zd > %u)\n", strlen(tag),
+                MAX_TAG_SIZE - 1);
         return 0;
     } else {
         memcpy(ctx->tag, tag, strlen(tag));
     }
 
     if (strlen(ifname) >= IFNAMSIZ) {
-        fprintf(stderr, "Interface name too long (%zd > %u\n", strlen(ifname),
-                IFNAMSIZ);
+        fprintf(stderr, "Interface name too long (%zd > %u)\n", strlen(ifname),
+                IFNAMSIZ - 1);
         return 0;
     } else {
         memcpy(ctx->ifname, ifname, strlen(ifname));
     }
 
     if (strlen(destination) >= MAX_ADDR_SIZE) {
-        fprintf(stderr, "Destination too long (%zd > %u\n", strlen(destination),
-                MAX_ADDR_SIZE);
+        fprintf(stderr, "Destination too long (%zd > %u)\n", strlen(destination),
+                MAX_ADDR_SIZE - 1);
         return 0;
     } else {
         memcpy(ctx->destination, destination, strlen(destination));
@@ -248,22 +277,22 @@ static uint8_t parse_cmd_args(struct tac_ctx *ctx, int argc, char *argv[])
             fprintf(stderr, "Address is not valid: %s\n", address);
             return 0;
         } else {
-            memcpy(ctx->destination, destination, strlen(destination));
+            memcpy(ctx->address, address, strlen(address));
         }
     } else if (ctx->addr_family == AF_INET6) {
         if (!inet_pton(AF_INET6, address, &addr_tmp)) {
             fprintf(stderr, "Address is not valid: %s\n", address);
             return 0;
         } else {
-            memcpy(ctx->destination, destination, strlen(destination));
+            memcpy(ctx->address, address, strlen(address));
         }
     } else {
         if (strlen(address) >= INET6_ADDRSTRLEN) {
-            fprintf(stderr, "Address too long (%zd > %u\n", strlen(address),
-                    INET6_ADDRSTRLEN);
+            fprintf(stderr, "Address too long (%zd > %u)\n", strlen(address),
+                    INET6_ADDRSTRLEN - 1);
             return 0;
         } else {
-            memcpy(ctx->destination, destination, strlen(destination));
+            memcpy(ctx->address, address, strlen(address));
         }
     }
 
