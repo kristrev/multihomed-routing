b5bc668d45297768a074d06b46ce7a59208ee873
Kristian Evensen <kristian.evensen@gmail.com>
2017-04-29 17:43:34 +0200

    [SRC] Check table on cache hit
    If we get a hit in our cache, make sure that table is set (or set the
    table). This is required in case for example syncing the in-memory table
    map and database fails when starting the server.

--- table_allocator/server/src/table_allocator_server_clients.c
+++ table_allocator/server/src/table_allocator_server_clients.c
@@ -90,6 +90,36 @@ static void set_table(struct tas_ctx *ctx, uint8_t addr_family,
     }
 }
 
+static uint8_t is_table_free(struct tas_ctx *ctx, uint8_t addr_family,
+        uint32_t rt_table)
+{
+    uint32_t element_index, element_bit, element_mask, element_masked;
+
+    //we do not add +1, since when using the normal bitwise operators we index
+    //at 0
+    rt_table = rt_table - ctx->table_offset;
+
+    element_index = rt_table >> 5;
+    //What we do here is to mask out the lowest five bits. They contain the
+    //index of the bit to be set (remember that 32 is 0x20);
+    element_bit = rt_table & 0x1F;
+
+    if (element_index >= ctx->num_table_elements)
+        return 0;
+
+    element_mask = 1 << element_bit;
+
+    if (addr_family == AF_INET) {
+        element_masked = ctx->tables_inet[element_index] & element_mask;
+    } else if (addr_family == AF_INET6) {
+        element_masked = ctx->tables_inet6[element_index] & element_mask;
+    } else {
+        element_masked = ctx->tables_unspec[element_index] & element_mask;
+    }
+
+    return !!element_masked;
+}
+
 static void release_dead_lease(void *ptr, uint8_t addr_family,
         uint32_t rt_table)
 {
@@ -152,7 +182,13 @@ uint8_t table_allocator_server_clients_handle_req(struct tas_ctx *ctx,
     rt_table_returned = table_allocator_sqlite_get_table(ctx, req);
 
     if (rt_table_returned) {
-        //check that table is set in map
+        //if for some reason the initial database read should fail, we need to
+        //update map when we find leases in the databases. There is no race with
+        //new leases. If we get the lease request for a new tuple, then the
+        //value stored in the db will (potentially) be overwritten
+        if (is_table_free(ctx, req->addr_family, rt_table_returned)) {
+            set_table(ctx, req->addr_family, rt_table_returned);
+        }
 
         //update lease, silently fail and trust client logic (for now)
         if (!table_allocator_sqlite_update_lease(ctx, rt_table_returned,
