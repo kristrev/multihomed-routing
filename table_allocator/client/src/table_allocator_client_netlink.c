470badf5da5b7ad6aaf85169dfc3a2f6787813b2
Kristian Evensen <kristian.evensen@gmail.com>
2017-05-04 17:38:52 +0200

    [FIX] Add license
    Table Allocator is licensed under GPLv3.

--- table_allocator/client/src/table_allocator_client_netlink.c
+++ table_allocator/client/src/table_allocator_client_netlink.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Kristian Evensen <kristian.evensen@gmail.com>
+ *
+ * This file is part of Usb Monitor. Usb Monitor is free software: you can
+ * redistribute it and/or modify it under the terms of the Lesser GNU General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Usb Montior is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Usb Monitor. If not, see http://www.gnu.org/licenses/.
+ */
+
 #include <uv.h>
 #include <string.h>
 #include <libmnl/libmnl.h>
